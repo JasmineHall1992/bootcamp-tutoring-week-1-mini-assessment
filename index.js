505bf9a3d45cc70fd41aa3127a204705410dba93
JasmineHall1992 <jasminehall5935@gmail.com>
2024-12-20 13:40:27 -0600

    jmh

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ Only declare your variables with const and let.
 */
 
 // Problem #1 //
-let getLastMedicationInfo 
+//I: an object called patient as the parameter
+//O: a string that includes the name of the LAST medication, the type and how frequently to take
+let getLastMedicationInfo = (patient) => {
+    //make variables to access the information
+   const lastMedicine = patient.medications[patient.medications.length - 1];
+   //the last medicine's name
+    const lastName = lastMedicine.name;
+    //the last medicine's type
+    const lastType = lastMedicine.type;
+    //the last medicines frequency
+    const instructions = lastMedicine.frequency;
+   //use a template literal to place together
+   const infoReturned = `${lastName} - ${lastType} - ${instructions}`;
+   return infoReturned;
+};
 
 // Problem #2 //
-let getObjectKeyValues 
+//I: one parameter that represents an array of patients
+//O: an array of strings
+let getObjectKeyValues = (patients) =>
+    // a backwads
 
 // Problem #3 //
 let getDailyMedications 
